6ad78e5734ceff4a3d787dc4de4b6f2cd95efb09
Frank-003 <oyiengofrankline49@gmail.com>
2024-05-08 01:27:56 -0700

    task 10

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -13,17 +13,30 @@
   *
   * Description: Prints the [sub]array being searched after each change.
   */
-int binary_search(int *array, int low, int high, int value) {
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return -1;
+int _binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
 
 /**
@@ -38,26 +51,21 @@ int binary_search(int *array, int low, int high, int value) {
   *
   * Description: Prints a value every time it is compared in the array.
   */
-int exponential_search(int *array, size_t size, int value) {
-    if (array == NULL || size == 0)
-        return -1;
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 0, right;
 
-    if (array[0] == value) {
-        printf("Comparing value %d with array[%d] = %d\n", value, 0, array[0]);
-        return 0;
-    }
+	if (array == NULL)
+		return (-1);
 
-    int i = 1;
-    while (i < size && array[i] <= value) {
-        printf("Comparing value %d with array[%d] = %d\n", value, i, array[i]);
-        i *= 2;
-    }
+	if (array[0] != value)
+	{
+		for (i = 1; i < size && array[i] <= value; i = i * 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
 
-    int low = i / 2;
-    int high = (i < size) ? i : size - 1;
-
-    printf("Exponential search range: [%d, %d]\n", low, high);
-
-    return binary_search(array, low, high, value);
+	right = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
+	return (_binary_search(array, i / 2, right, value));
 }
 
